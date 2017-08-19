448cdeeadbced3a2663b3a0389a78fd1daf6f0d2
Abdul Rehman Khawar <designbysoil@gmail.com>
2017-08-19 17:14:22 +0300

    Initial commit. Creating a new application using create-react-app.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class BooksApp extends Component {
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    );
+  }
+}
+
+export default BooksApp;
